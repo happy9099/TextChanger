413e5142763471fb076e2f5f971db542c65c01d4
Happy <hgp9825@gmail.com>
2023-03-13 10:06:34 +0530

    Video 15: Changing title dynemically & adding favicon

--- src/App.js
+++ src/App.js
@@ -28,11 +28,24 @@ function App() {
       setMode('dark');
       document.body.style.backgroundColor = '#002040';
       showAlert("Dark mode has been enabled", "success");
+      // Written on the top beside favicon
+      // document.title = "TextChanger - Dark Mode"
+      // For evil app
+      // setInterval(() => {
+      //   document.title = "Install TextChanger YEAH"
+
+      // },1000) 
     }
     else {
       setMode('light');
       document.body.style.backgroundColor = 'white';
       showAlert("Light mode has been enabled", "success");
+      // Written on the top beside favicon
+      // document.title = "TextChanger - Light Mode" 
+      // setInterval(() => {
+      //   document.title = "Install TextChanger Now "
+  
+      // },1000) 
     }
   }
 
